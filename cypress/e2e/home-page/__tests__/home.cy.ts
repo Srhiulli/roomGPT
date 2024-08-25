e22f4c8f1199e8b61c0d3f2a06e2fd589e71823a
Srhiulli <srhiulli@gmail.com>
2024-08-25 18:22:54 -0300

    cypress initiation/ first home page test

---
+++ cypress/e2e/home-page/__tests__/home.cy.ts
@@ -0,0 +1,30 @@
+
+import { homepageSelectors } from '../pages/home-page';
+
+describe('HomePage Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should display the header and footer', () => {
+    cy.get(homepageSelectors.header).should('exist');
+    cy.get(homepageSelectors.footer).should('exist');
+  });
+
+  it('Should display the correct title and description', () => {
+    cy.get(homepageSelectors.title).contains('Generating dream rooms using AI').should('be.visible');
+    cy.get(homepageSelectors.description).contains('Take a picture of your room and see how your room looks in different themes.')
+      .should('be.visible');
+  });
+
+  it('Should have a working link to Vercel and Dream page', () => {
+    cy.get(homepageSelectors.vercelLink).should('have.attr', 'target', '_blank');
+    cy.get(homepageSelectors.dreamButton).click();
+    cy.url().should('include', '/dream');
+  });
+
+  it('Should display the images correctly', () => {
+    cy.get(homepageSelectors.originalRoomImage).should('be.visible');
+    cy.get(homepageSelectors.generatedRoomImage).should('be.visible');
+  });
+});
